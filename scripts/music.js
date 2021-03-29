6d2d400e5c8a47a3d61ef79f0275b1d4cb2bdc72
HughJazz123 <50455574+HughJazz123@users.noreply.github.com>
2021-03-29 12:34:41 +0800

    updated music audio feature

--- scripts/music.js
+++ scripts/music.js
@@ -6,23 +6,28 @@ $(document).ready(function (){
     var note_g = $('#G')[0];
     var note_C1 = $('#C1')[0];
     var note_E1 = $('#E1')[0];
-    $('#m').mouseenter(function(){
+    $('#m').click(function(){
+        note_c.currentTime = 0;
         note_c.play();
     });
 
-    $('#u').mouseenter(function(){
+    $('#u').click(function(){
+        note_e.currentTime = 0;
         note_e.play();
     });
 
-    $('#s').mouseenter(function(){
+    $('#s').click(function(){
+        note_g.currentTime = 0;
         note_g.play();
     });
 
-    $('#i').mouseenter(function(){
+    $('#i').click(function(){
+        note_C1.currentTime = 0;
         note_C1.play();
     });
 
-    $('#c').mouseenter(function(){
+    $('#c').click(function(){
+        note_E1.currentTime = 0;
         note_E1.play();
     });
 })
