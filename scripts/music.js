d6b8b1aad1f81e3285f6b8bdf6ee9e8fd79022e5
HughJazz123 <50455574+HughJazz123@users.noreply.github.com>
2021-04-29 10:44:38 +0800

    removed shop section and modified music

--- scripts/music.js
+++ scripts/music.js
@@ -2,9 +2,9 @@
 
 $(document).ready(function (){
     var note_c = $('#C')[0];
-    var note_e = $('#E')[0];
     var note_g = $('#G')[0];
     var note_C1 = $('#C1')[0];
+    var note_D1 = $('#D1')[0];
     var note_E1 = $('#E1')[0];
     $('#m').click(function(){
         note_c.currentTime = 0;
@@ -12,18 +12,18 @@ $(document).ready(function (){
     });
 
     $('#u').click(function(){
-        note_e.currentTime = 0;
-        note_e.play();
-    });
-
-    $('#s').click(function(){
         note_g.currentTime = 0;
         note_g.play();
     });
 
-    $('#i').click(function(){
+    $('#s').click(function(){
         note_C1.currentTime = 0;
         note_C1.play();
+    });
+
+    $('#i').click(function(){
+        note_D1.currentTime = 0;
+        note_D1.play();
     });
 
     $('#c').click(function(){
